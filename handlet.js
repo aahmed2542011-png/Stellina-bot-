9ce5cd2da67c60c4ae7e97372861be3ff6250823
aahmed2542011-png <a.ahmed2542011@fadzanardelli.it>
2026-04-18 20:06:25 +0200

    Update handlet.js

--- handlet.js
+++ handlet.js
@@ -1,5 +1,12 @@
 const handler = async (m, { conn }) => {
-   ...
+
+    const chat = m.chat;
+    const text = (m.text || '').toLowerCase();
+
+    if (text === '!ping') {
+        return conn.sendMessage(chat, { text: 'pong 🏓' }, { quoted: m });
+    }
+
 };
 
 handler.all = true;
